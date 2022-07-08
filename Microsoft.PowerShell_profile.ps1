e3ae7bb61176df766f6503654011497a8eaa49ee
rhydon <adirmak1@gmail.com>
2022-07-08 21:36:41 +0300

    git commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,7 @@ Import-Module oh-my-posh
 Set-PoshPrompt -Theme free-ukraine
 
 Set-Alias -Name vscode -Value code # Make sure vscode dir is in your PATH environment variable
+
 function codedir {
     param ()
     Set-Location C:\Users\rhydon\Desktop
@@ -25,3 +26,15 @@ function search {
         Start-Process "C:\Program Files\Everything\Everything.exe"
     }
 }
+
+# Git commands
+function commit {
+    param (
+        [Parameter(Mandatory=$true)]$commitMessage,
+        [Parameter(Mandatory=$false)]$extended
+    )
+    Invoke-Expression -Command "git add . ; git commit -m `"$commitMessage`" $extended"
+}
+
+function status { git status }
+function push { git push }
